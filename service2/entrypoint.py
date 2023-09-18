d8933e6169dedac73b3f6dc3718c86830de55317
Marko <marko.kremer@fer.hr>
2023-09-18 02:05:51 +0200

    changed service2 code

--- service2/entrypoint.py
+++ service2/entrypoint.py
@@ -1,15 +1,21 @@
 import requests
 import sys
 import os
+import validators
 
-SERVICE1_URL = os.getenv('SERVICE1')
+page_url = sys.stdin.readline().strip()
 
-message = requests.get(sys.stdin.readline()).text
-data = ["md5", message]
+if validators.url(page_url):
+    SERVICE1_URL = os.getenv('SERVICE1')
 
-response = requests.post(SERVICE1_URL, data="\n".join(data))
+    message = requests.get(page_url).text
+    data = ["md5", message]
 
-if response.status_code == 200:
-    print(response.text)
+    response = requests.post(SERVICE1_URL, data="\n".join(data).encode("utf-8"))
+
+    if response.status_code == 200:
+        print(response.text)
+    else:
+        print("Service 1 not responding")
 else:
-    print("Service 1 not responding")
+    print("Please provide a valid URL")
